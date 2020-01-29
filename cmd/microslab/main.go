517fbd67ae1f48288888566b49f0c91597d76c8e
tapokshot <tapokshot@gmail.com>
2020-01-29 22:46:04 +0300

    init

---
+++ cmd/microslab/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "flag"
+    "github.com/BurntSushi/toml"
+    "github.com/tapokshot/microslab/internal/app/microslab"
+    "log"
+)
+
+var (
+    configPath string
+)
+
+func init() {
+    flag.StringVar(&configPath, "config-path", "configs/application.toml", "Application config")
+}
+
+func main() {
+    flag.Parse()
+
+
+    config := microslab.CreateConfig()
+    _, err := toml.DecodeFile(configPath, &config)
+    if err != nil {
+        log.Fatal(err)
+    }
+    launcher := microslab.New(config)
+
+    if err := launcher.Launch(); err != nil {
+        log.Fatal(err)
+    }
+
+}
---
+++ internal/app/microslab/config.go
@@ -0,0 +1,14 @@
+package microslab
+
+// Server config
+type Config struct {
+    BindAddr string `toml:"bind_addr"`
+    LogLevel string `toml:"log_level"`
+}
+
+func CreateConfig() *Config {
+    return &Config{
+        BindAddr: "8080",
+        LogLevel: "debug",
+    }
+}
